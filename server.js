112025fd344fb8cb1230a8f3dcff68f7d920f8ee
Sahit Katta <sahit.mohan@gmail.com>
2024-01-12 19:39:38 -0600

    disable grahpiql

--- server.js
+++ server.js
@@ -89,11 +89,10 @@ const app = express();
 app.use('/', graphqlHTTP({
   schema,
   rootValue: root,
-  graphiql: true, // Enable GraphiQL interface
 }));
 
 // Start the server
 const port = process.env.PORT || 8080
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/graphql`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
